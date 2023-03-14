f356e2b9f40e2b783be6070c16dab6eb21cb8acd
Alexander Kosachev <kosachev_alex@mail.ru>
2023-03-14 03:26:24 +0300

    run

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"log"
 	"os"
-	"path"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/goccy/go-json"
 )
@@ -17,15 +19,23 @@ type (
 	}
 
 	Action struct {
-		Data any
+		Data     any
+		Parallel bool
 	}
 
 	Type struct {
-		Type string `json:"type"`
+		Type     string `json:"type"`
+		Parallel bool   `json:"parallel"`
 	}
 
 	Copy struct {
 		File string `json:"file"`
+	}
+
+	Run struct {
+		Path            string   `json:"path"`
+		EnvironmentList []string `json:"environments"`
+		ArgumentList    []string `json:"arguments"`
 	}
 )
 
@@ -37,20 +47,19 @@ func (action *Action) UnmarshalJSON(source []byte) error {
 		return err
 	}
 
+	action.Parallel = t.Parallel
+
 	switch t.Type {
+	case "run":
+		action.Data = new(Run)
 	case "copy":
 		action.Data = new(Copy)
-
-		err = json.Unmarshal(source, action.Data)
 	default:
 		action.Data = "empty"
+		return nil
 	}
 
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return json.Unmarshal(source, action.Data)
 }
 
 func main() {
@@ -97,6 +106,8 @@ func (deploy *Deploy) Do(action int) error {
 	data := deploy.ActionList[action].Data
 
 	switch data.(type) {
+	case *Run:
+		return deploy.Run(data.(*Run))
 	case *Copy:
 		return deploy.Copy(data.(*Copy))
 	default:
@@ -106,13 +117,49 @@ func (deploy *Deploy) Do(action int) error {
 	return nil
 }
 
+func (deploy *Deploy) Run(run *Run) error {
+	if filepath.Base(run.Path) == run.Path {
+		path, err := exec.LookPath(run.Path)
+		if err != nil {
+			return err
+		}
+
+		run.Path = path
+	}
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+
+	command := &exec.Cmd{
+		Path: run.Path,
+		Env:  append(os.Environ(), run.EnvironmentList...),
+		Args: append([]string{run.Path}, run.ArgumentList...),
+
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+
+	err := command.Run()
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stdout.ReadFrom(stdout)
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stderr.ReadFrom(stderr)
+	return err
+}
+
 func (deploy *Deploy) Copy(copy *Copy) error {
 	source, err := os.Open(copy.File)
 	if err != nil {
 		return err
 	}
 
-	target, err := os.Create(path.Join(deploy.Folder, source.Name()))
+	target, err := os.Create(filepath.Join(deploy.Folder, source.Name()))
 	if err != nil {
 		return err
 	}
